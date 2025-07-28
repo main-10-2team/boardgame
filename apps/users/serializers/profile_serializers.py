c27a52b81730096ae47c8a279556fd53c90151a7
CJY9697 <jy970114@naver.com>
2025-07-28 19:54:27 +0900

    ♻️ refactor: 프로필 조회 수정, url 추가

--- apps/users/serializers/profile_serializers.py
+++ apps/users/serializers/profile_serializers.py
@@ -4,20 +4,35 @@ from apps.users.models import User
 
 
 class UserProfileSerializer(serializers.ModelSerializer[User]):
-    user_id = serializers.IntegerField(source="id")
+    preferred_genres = serializers.SerializerMethodField()
+    preferred_playtimes = serializers.SerializerMethodField()
+    review_count = serializers.SerializerMethodField()
+    like_count = serializers.SerializerMethodField()
 
     class Meta:
         model = User
         fields = [
-            "user_id",
+            "name",
             "email",
             "nickname",
-            "birth",
-            "phone_number",
-            "role",
-            "status",
+            "profile_image",
+            "review_count",
+            "like_count",
             "created_at",
-            "updated_at",
+            "preferred_genres",
+            "preferred_playtimes",
         ]
 
         read_only_fields = fields
+
+    def get_review_count(self, obj: User) -> int:
+        return obj.reviews.all().count()
+
+    def get_like_count(self, obj: User) -> int:
+        return obj.likes.all().count()
+
+    def get_preferred_genres(self, obj: User) -> list[str]:
+        return [genre.name for genre in obj.preferred_genres.all()]
+
+    def get_preferred_playtimes(self, obj: User) -> list[str]:
+        return [pt.name for pt in obj.preferred_playtimes.all()]

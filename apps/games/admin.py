b3abb1d1b4b920b150306bd3f9313e0578a9f7b2
CJY9697 <jy970114@naver.com>
2025-07-30 17:12:02 +0900

    💡 chore : 코드포매팅

--- apps/games/admin.py
+++ apps/games/admin.py
@@ -4,7 +4,7 @@ from apps.games.models import PlaytimeCategory
 
 
 @admin.register(PlaytimeCategory)
-class PlaytimeCategoryAdmin(admin.ModelAdmin): # type: ignore[type-arg]
+class PlaytimeCategoryAdmin(admin.ModelAdmin):  # type: ignore[type-arg]
     list_display = ("playtime_id", "name", "min_minutes", "max_minutes")
     search_fields = ("name",)
     ordering = ("min_minutes",)

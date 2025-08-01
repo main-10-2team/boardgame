f6c86f68478ece3adedbc3c88ef28d68dd32c18a
enjore <shawn630730@gmail.com>
2025-08-01 17:09:56 +0700

    💡 chore    : url 추가

--- apps/games/urls.py
+++ apps/games/urls.py
@@ -4,6 +4,7 @@ from apps.games.views.admin_review_management_view import (
     AdminReviewDeleteView,
     AdminReviewListview,
 )
+from apps.games.views.game_detail_view import GameDetailView
 from apps.games.views.game_filter_view import GameFilterView
 from apps.games.views.game_list_view import GameListView
 from apps.games.views.game_search_view import GameSearchView
@@ -12,6 +13,7 @@ urlpatterns = [
     path("games/", GameListView.as_view(), name="game-list"),
     path("games/search", GameSearchView.as_view(), name="game-search"),
     path("games/filter", GameFilterView.as_view(), name="game-filter"),
+    path("games/<int:game_id>", GameDetailView.as_view(), name="game-detail"),
     path("admin/reviews/", AdminReviewListview.as_view(), name="admin-review-list"),
     path("admin/reviews/<int:review_id>/", AdminReviewDeleteView.as_view(), name="admin_review_delete"),
 ]

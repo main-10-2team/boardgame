f212b718e02c95ce2309ffba12832a95be286938
enjore <shawn630730@gmail.com>
2025-08-01 16:58:12 +0700

    💡 chore    : ci 수정

--- apps/users/serializers/auth/auth_logout_serializers.py
+++ apps/users/serializers/auth/auth_logout_serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 
 
-class LogoutSerializer(serializers.Serializer):# type: ignore
+class LogoutSerializer(serializers.Serializer):  # type: ignore
     refresh_token = serializers.CharField(help_text="JWT 리프레시 토큰", write_only=True)
 
 
-class LogoutResponseSerializer(serializers.Serializer):# type: ignore
+class LogoutResponseSerializer(serializers.Serializer):  # type: ignore
     status = serializers.CharField()
     message = serializers.CharField()

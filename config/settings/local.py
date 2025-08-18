10e24121bd0bf2949b835d46ec94eceddcbef436
CJY9697 <jy970114@naver.com>
2025-08-18 14:42:57 +0900

    💡 chore    : 세팅 수정은 나중에 다시 변경

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,12 +1,7 @@
 from config.settings.base import *
 
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ["*"]
-
-INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + OWNER_APPS
-INSTALLED_APPS.remove("debug_toolbar")
-
-MIDDLEWARE = [m for m in MIDDLEWARE if "debug_toolbar" not in m]
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "static/"
@@ -18,18 +13,3 @@ MEDIA_ROOT = BASE_DIR / "media"
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
-
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {
-        "console": {
-            "level": "INFO",
-            "class": "logging.StreamHandler",
-        },
-    },
-    "root": {
-        "handlers": ["console"],
-        "level": "INFO",
-    },
-}
